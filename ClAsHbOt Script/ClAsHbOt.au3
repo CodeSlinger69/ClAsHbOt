CodeSlinger69/ClAsHbOt

commit 7df0d1b3a94fd55f48989d4e7c6c78b6c30c0f95

Fix for capturing my cups and my dark if dark elixir bar is not present.

ClAsHbOt Script/ClAsHbOt.au3
@@ -256,13 +256,19 @@ Func GetMyLootNumbers()
 	  ; Scrape text fields
 	  Local $MyGold = Number(ScrapeFuzzyText($gSmallCharacterMaps, $rMyGoldTextBox, $gSmallCharMapsMaxWidth, $eScrapeDropSpaces))
 	  Local $MyElix = Number(ScrapeFuzzyText($gSmallCharacterMaps, $rMyElixTextBox, $gSmallCharMapsMaxWidth, $eScrapeDropSpaces))
-	  Local $MyDark = Number(ScrapeFuzzyText($gSmallCharacterMaps, $rMyDarkTextBox, $gSmallCharMapsMaxWidth, $eScrapeDropSpaces))
-	  Local $MyGems
+
+	  Local $MyDark = 0
+	  If IsTextBoxPresent($rMyDarkTextBox) Then
+		 Number(ScrapeFuzzyText($gSmallCharacterMaps, $rMyDarkTextBox, $gSmallCharMapsMaxWidth, $eScrapeDropSpaces))
+	  EndIf
+
+	  Local $MyGems = 0
 	  If IsTextBoxPresent($rMyGemsTextBoxNoDE) = True Then
 		 $MyGems = Number(ScrapeFuzzyText($gSmallCharacterMaps, $rMyGemsTextBoxNoDE, $gSmallCharMapsMaxWidth, $eScrapeDropSpaces))
 	  Else
 		 $MyGems = Number(ScrapeFuzzyText($gSmallCharacterMaps, $rMyGemsTextBoxWithDE, $gSmallCharMapsMaxWidth, $eScrapeDropSpaces))
 	  EndIf
+
 	  GUICtrlSetData($GUI_MyGold, $MyGold)
 	  GUICtrlSetData($GUI_MyElix, $MyElix)
 	  GUICtrlSetData($GUI_MyDark, $MyDark)

ClAsHbOt Script/RegionDefs.au3
@@ -3,9 +3,9 @@
 ; Present indicator x, y, color, radius
 Global $rGoldTextBox[10] = [42, 57, 120, 74, 0xfffbcc, 9, 30, 66, 0xeace3f, 6]
 Global $rElixTextBox[10] = [42, 81, 120, 98, 0xffe8fd, 9, 30, 93, 0xd958d0, 6]
-Global $rDarkTextBox[10] = [42, 107, 120, 124, 0xf3f3f3, 9, 30, 117, 0x53435f, 6]
-Global $rCupsTextBox1[10] = [42, 107, 120, 124, 0xffffff, 9, 30, 117, 0xe7d99b, 6]
-Global $rCupsTextBox2[10] = [42, 140, 120, 157, 0xffffff, 9, 30, 141, 0xf0e883, 6]
+Global $rDarkTextBox[10] = [42, 107, 120, 124, 0xf3f3f3, 9, 30, 117, 0x53425f, 6]
+Global $rCupsTextBox1[10] = [42, 118, 120, 131, 0xffffff, 9, 30, 117, 0xe7d99b, 6]
+Global $rCupsTextBox2[10] = [42, 144, 120, 157, 0xffffff, 9, 30, 141, 0xf0e883, 6]
 
 Global $rMyGoldTextBox[10] = [895, 20, 976, 31, 0xffffff, 9, 990, 24, 0xF3EC54, 6]
 Global $rMyElixTextBox[10] = [895, 64, 976, 75, 0xffffff, 9, 0, 0, 0, 0]

ClAsHbOt Script/AutoRaid.au3
@@ -261,13 +261,14 @@ Func CheckForRaidableBase()
    ; Scrape info
    Local $gold = Number(ScrapeFuzzyText($gRaidLootCharMaps, $rGoldTextBox, $gRaidLootCharMapsMaxWidth, $eScrapeDropSpaces))
    Local $elix = Number(ScrapeFuzzyText($gRaidLootCharMaps, $rElixTextBox, $gRaidLootCharMapsMaxWidth, $eScrapeDropSpaces))
-   Local $dark = Number(ScrapeFuzzyText($gRaidLootCharMaps, $rDarkTextBox, $gRaidLootCharMapsMaxWidth, $eScrapeDropSpaces))
+   Local $dark = 0
    Local $cups = 0
    Local $townHall = -1
 
-   If IsTextBoxPresent($rCupsTextBox1) Then
+   If IsTextBoxPresent($rDarkTextBox)=False Then
 	  $cups = Number(ScrapeFuzzyText($gRaidLootCharMaps, $rCupsTextBox1, $gRaidLootCharMapsMaxWidth, $eScrapeDropSpaces))
-   ElseIf IsTextBoxPresent($rCupsTextBox2) Then
+   Else
+	  $dark = Number(ScrapeFuzzyText($gRaidLootCharMaps, $rDarkTextBox, $gRaidLootCharMapsMaxWidth, $eScrapeDropSpaces))
 	  $cups = Number(ScrapeFuzzyText($gRaidLootCharMaps, $rCupsTextBox2, $gRaidLootCharMapsMaxWidth, $eScrapeDropSpaces))
    EndIf
 

commit 7c79dcc363aa49edb6e7ac4ff2a8a6209aac7ab7

Fix for regression introduced in 7df0d1b3a94fd55f48989d4e7c6c78b6c30c0f95
MyDark correctly scraped again.

ClAsHbOt Script/ClAsHbOt.au3
@@ -258,8 +258,8 @@ Func GetMyLootNumbers()
 	  Local $MyElix = Number(ScrapeFuzzyText($gSmallCharacterMaps, $rMyElixTextBox, $gSmallCharMapsMaxWidth, $eScrapeDropSpaces))
 
 	  Local $MyDark = 0
-	  If IsTextBoxPresent($rMyDarkTextBox) Then
-		 Number(ScrapeFuzzyText($gSmallCharacterMaps, $rMyDarkTextBox, $gSmallCharMapsMaxWidth, $eScrapeDropSpaces))
+	  If IsTextBoxPresent($rMyGemsTextBoxWithDE) = True Then
+		 $MyDark = Number(ScrapeFuzzyText($gSmallCharacterMaps, $rMyDarkTextBox, $gSmallCharMapsMaxWidth, $eScrapeDropSpaces))
 	  EndIf
 
 	  Local $MyGems = 0

ClAsHbOt Script/RegionDefs.au3
@@ -11,7 +11,7 @@ Global $rMyGoldTextBox[10] = [895, 20, 976, 31, 0xffffff, 9, 990, 24, 0xF3EC54, 
 Global $rMyElixTextBox[10] = [895, 64, 976, 75, 0xffffff, 9, 0, 0, 0, 0]
 Global $rMyDarkTextBox[10] = [895, 106, 976, 117, 0xffffff, 9, 0, 0, 0, 0]
 Global $rMyGemsTextBoxNoDE[10] = [917, 106, 976, 117, 0xffffff, 9, 903, 112, 0xFFFFFF, 0]
-Global $rMyGemsTextBoxWithDE[10] = [917, 147, 976, 158, 0xffffff, 9, 0, 0, 0, 0]
+Global $rMyGemsTextBoxWithDE[10] = [917, 147, 976, 158, 0xffffff, 9, 903, 153, 0xFFFFFF, 0]
 Global $rMyCupsTextBox[10] = [50, 64, 104, 74, 0xffffff, 9, 0, 0, 0, 0]
 
 Global $rBarracksWindowTextBox[10] = [262, 113, 395, 127, 0xffffff, 9, 0, 0, 0, 0]